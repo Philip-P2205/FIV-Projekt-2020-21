52b26fdf54bf52b78f8f5e0bd2c51ebbc652e847
dapossi <64413225+dapossi@users.noreply.github.com>
2021-06-10 14:53:41 +0200

    PS/2 memory map setup

---
+++ Library/ps2.h
@@ -0,0 +1,16 @@
+/*
+ * ps2.h
+ *
+ *  Created on: 27.05.2021
+ *      Author: jakob
+ */
+
+#ifndef LIBRARY_PS2_H_
+#define LIBRARY_PS2_H_
+
+extern char *ps2_map;
+extern char *ps2_map_shifted;
+
+
+
+#endif /* LIBRARY_PS2_H_ */
