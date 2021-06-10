b7c42f3a48c73edd4760a095e09488c9b8a56651
pipip <pipip@10.101.3.13>
2021-06-10 15:12:13 +0200

    PS/2 Keymap interface

--- Library/ps2.h
+++ Library/ps2.h
@@ -8,9 +8,16 @@
 #ifndef LIBRARY_PS2_H_
 #define LIBRARY_PS2_H_
 
-extern char *ps2_map;
-extern char *ps2_map_shifted;
+#include <stdint.h>
 
+#define PS2_SHIFT_PRESSED_STATE 0x01
+
+extern uint8_t ps2_states;
+extern uint8_t *ps2_map;
+extern uint8_t *ps2_map_shifted;
+extern uint8_t ps2_buffer[256];
+
+extern uint8_t ps2KeyboardChar(uint8_t scancode);
 
 
 #endif /* LIBRARY_PS2_H_ */
